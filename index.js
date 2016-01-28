60f8da666bc382d470742a5e5ad17845c4602cbd
Taylor Mock <tmock12@gmail.com>
2016-01-28 09:50:12 -0500

    Only import stagehand assets in non production environments

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 module.exports = {
   name: 'ember-stagehand',
   included: function(app) {
-      this._super.included(app);
+    this._super.included(app);
+
+    if (app.env !== 'production') {
       app.import('vendor/stagehand/stagehand.js');
       app.import('vendor/stagehand/stagehand.css');
     }
+  }
 };
